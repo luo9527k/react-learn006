be5dfc1341b4952283d7a7988c20cd2c93c20f81
luo9527k <105136804+luo9527k@users.noreply.github.com>
2023-05-31 14:48:30 +0800

    1 file modified, 3 deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import React from 'react'; //语法核心
 import ReactDOM from 'react-dom/client'; //渲染核心
-import Test from './test/Test';
-// import Demo from './views/Demo';
-// import Index from './views/Index';
-// import './views/Index.less';
 
 //获取id为root的容器，作为我们的根容器
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,8 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //基于数据的值，来判断元素的显示与隐藏
 root.render(
   <>
-    {/* <Demo /> */}
-    <Test />
-    {/* <Index /> */}
+  
   </>
 );
