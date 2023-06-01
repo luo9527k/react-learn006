f3ac26cae3471c4dc34607a55c9e6b0dbe114ebc
luo9527k <105136804+luo9527k@users.noreply.github.com>
2023-06-01 18:28:19 +0800

    Task demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'; //语法核心
 import ReactDOM from 'react-dom/client'; //渲染核心
-import Test from './test/Test';
+import Task from './views/Task';
+//使用ANTD组件库
+import { ConfigProvider } from 'antd'; //使用汉化包
+import zhCN from 'antd/locale/zh_CN';
+//导入全局样式
+import './main.less';
 
 //获取id为root的容器，作为我们的根容器
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //基于数据的值，来判断元素的显示与隐藏
 root.render(
-  <>
-    <Test />
-  </>
+  <ConfigProvider locale={zhCN}>
+    <Task />
+  </ConfigProvider>
 );
---
+++ src/views/Task.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import './Task.less';
+//解构ANTD的组件
+import { DatePicker, Button, Tag, Table, Popconfirm } from 'antd';
+
+class Task extends React.Component {
+  // 表格数据
+  columns = [
+    {
+      title: '编号',
+      dataIndex: 'id',
+      align: 'center',
+    },
+    {
+      title: '作品',
+      dataIndex: 'title',
+      align: 'center',
+    },
+    {
+      title: '作者',
+      dataIndex: 'author',
+      align: 'center',
+    },
+    {
+      title: '完成时间',
+      dataIndex: 'time',
+    },
+    {
+      title: '状态',
+      dataIndex: 'state',
+      align: 'center',
+      //text表示当前这一列，record这一行的完整数据
+      render: (text, record) => {
+        return text === 0 ? '未完成' : '已完成';
+      },
+    },
+    {
+      title: '操作',
+      align: 'center',
+      //对数据进行操作使用render函数进行渲染
+      render: (_, record) => {
+        const { state } = record;
+        return (
+          <>
+            <Popconfirm
+              title="此操作为删除"
+              description="您确认需要删除"
+              onConfirm={confirm}
+              // onCancel={cancel}
+              okText="是"
+              cancelText="否"
+            >
+              <Button danger size="small">
+                删除
+              </Button>
+            </Popconfirm>
+            {state !== 1}
+            {
+              <Popconfirm
+                title="确认"
+                onConfirm={confirm}
+                // onCancel={cancel}
+                okText="是"
+                cancelText="否"
+              >
+                <Button size="small">完成</Button>
+              </Popconfirm>
+            }
+          </>
+        );
+      },
+    },
+  ];
+  //编写一些数据
+  data = {
+    tableData: [
+      {
+        id: 1,
+        title: '泰酷了',
+        author: '王林凯',
+        state: 1, //完成状态
+        time: '2023-6-1',
+      },
+      {
+        id: 2,
+        title: '你干嘛',
+        author: '蔡徐坤',
+        state: 0, //完成状态
+        time: '2023-5-29',
+      },
+      {
+        id: 3,
+        title: '九转大肠',
+        author: '姜涛',
+        state: 1, //完成状态
+        time: '2023-5-28',
+      },
+    ],
+    tableLoading: false,
+  };
+  render() {
+    //解构值
+    const { tableData, tableLoading } = this.data;
+    return (
+      <>
+        <div className="task-box">
+          {/* 头部 */}
+          <div className="header">
+            <div className="title">TASK管理系统</div>
+            <Button type="primary">新增任务</Button>
+          </div>
+          <div className="tag-box">
+            <div className="tag">
+              <Tag color="blue">全部</Tag>
+              <Tag color="volcano">未完成</Tag>
+              <Tag color="purple">已完成</Tag>
+            </div>
+            <div className="time">
+              <DatePicker />
+            </div>
+          </div>
+          {/* 表格 */}
+          <Table
+            dataSource={tableData}
+            columns={this.columns}
+            loading={tableLoading}
+            rowKey="id"
+          />
+          {/* 对话框&表单 */}
+        </div>
+      </>
+    );
+  }
+}
+export default Task;
