1495c5ea0bc9ac5ec5b4d34ad729d8aef18bce68
luo9527k <105136804+luo9527k@users.noreply.github.com>
2023-06-06 17:24:19 +0800

    异步改同步

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'; //语法核心
 import ReactDOM from 'react-dom/client'; //渲染核心
 import Test from '../src/views/Test';
 import Root from '../src/views/Root';
-import Vote from '../src/views/Vote';
-import Vote2 from '../src/views/Vote2';
+// import Vote from '../src/views/Vote';
+// import Vote2 from '../src/views/Vote2';
+import Position from '../src/views/Position';
 import { ConfigProvider } from 'antd';
 
 //获取id为root的容器，作为我们的根容器
@@ -14,7 +15,8 @@ root.render(
   <ConfigProvider>
     <Test />
     <Root />
-    <Vote title="全民投票" />
-    <Vote2 title="投票器" />
+    <Position />
+    {/* <Vote title="全民投票" />
+    <Vote2 title="投票器" /> */}
   </ConfigProvider>
 );
---
+++ src/views/Position.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+//该函数可以使函数同步执行
+import { flushSync } from 'react-dom';
+import { Button } from 'antd';
+
+const Demo = (props) => {
+  console.log('render渲染');
+  const [x, setX] = useState(10),
+    [y, setY] = useState(20),
+    [z, setZ] = useState(30);
+
+  const Btn = () => {
+    // setTimeout(() => {
+    //   setX(x + 10);
+    //   setY(y + 10);
+    // }, 2000);
+
+    flushSync(() => {
+      setX(x + 10);
+      setY(y + 10);
+    });
+    setZ(z + 10);
+  };
+  return (
+    <>
+      <div>x:{x}</div>
+      <div>y:{y}</div>
+      <div>z:{z}</div>
+
+      <Button type="primary" onClick={Btn}>
+        异步
+      </Button>
+    </>
+  );
+};
+
+export default Demo;
