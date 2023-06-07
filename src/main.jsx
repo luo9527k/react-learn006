b7d58d9635594a3211ec65218a09092f5b789255
luo9527k <105136804+luo9527k@users.noreply.github.com>
2023-06-07 20:12:46 +0800

    add

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import ReactDOM from 'react-dom/client'; //渲染核心
 // import Demo from '../src/views/Demo';
 // import Demo2 from '../src/views/Demo2';
 // import Index from '../src/Test/Index';
-// import Page from '../src/pages/Page';
-import Page2 from '../src/pages/Page2';
+import Page from '../src/pages/Page';
+// import Page2 from '../src/pages/Page2';
 import { ConfigProvider } from 'antd';
 
 //获取id为root的容器，作为我们的根容器
@@ -27,7 +27,7 @@ root.render(
     {/* 将参数传递给组件 */}
     {/* <Demo2 x="10" y="20" /> */}
     {/* <Index title="Index组件" /> */}
-    {/* <Page /> */}
-    <Page2 />
+    <Page />
+    {/* <Page2 /> */}
   </ConfigProvider>
 );
--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 
 /*
   useEffect:在函数组件中，使用生命周期函数
@@ -42,13 +42,21 @@ const Page = () => {
     };
   }, [num]);
 
+  useLayoutEffect(() => {
+    console.log('useLayoutEffect');
+  }, [num]);
+
+  useEffect(() => {
+    console.log('useEffect');
+  });
+
   const handle = () => {
     setNum(num + 1);
   };
   return (
     <>
       <div>
-        <span>{num}</span>
+        <span style={{ color: num === 0 ? 'red' : 'green' }}>{num}</span>
         <button onClick={handle}>增加</button>
       </div>
     </>
@@ -56,3 +64,26 @@ const Page = () => {
 };
 
 export default Page;
+
+/*
+如果链表中的callback执行又修改了状态值[视图更新]，对于useEffect来讲：第一次真实DOM已经渲染，组件更新会重新渲染真实的DOM；所以频繁切换的时候，会出现样式内容闪烁
+
+对于 useLayoutEffect 来讲：第一次真实DOM还未渲染，遇到callback中修改了状态，视图立即更新，创建出新的 virtualDOM 然后和上一次的virtualDOM合并在一起渲染为真实DOM；也就是此类需求下，真实DOM只会渲染一次
+
+useLayoutEffect 会阻塞浏览器渲染真实DOM，优先执行Effect链表中的callback；
+useEffect 不会阻塞浏览器渲染真实DOM，在渲染真实DOM的同时，去执行Effect链表中的callback
+  - useLayoutEffect 设置的callback要优先于useEffect去执行
+  - 两者设置的callback 中依旧可以获取DOM元素[原因：真实DOM对象已经创建了，区别只是浏览器是否渲染]
+   - 如果在callback函数中又修改了状态值[视图又要更新]
+   - useEffect：浏览器肯定是肯定是一把第一次真实已经绘制，在渲染第二次真实DOM
+   - useLayoutEffect：浏览器是把两次DOM的渲染，合并在一起渲染的
+
+
+视图更新的步骤：
+  第一步：基于babel-presel-react-app把JSX编译createElement格式
+  第二步：把createElement执行，创建 virtualDOM
+  第三步：基于root.render方法 virtualDOM 变为真实DOM对象[DOM-DIFF]
+        useLayoutEffect阻塞第四步操作，先去执行 Effect 链表中方法[同步操作]
+        useEffect第四步操作和Effect链表中的方法执行，是同步进行的[异步操作]
+  第四步：浏览器渲染和绘制真实DOM对象
+*/
