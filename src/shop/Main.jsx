30f86c64fb68810921eb0b6940c48d39c7d9cea4
luo9527k <105136804+luo9527k@users.noreply.github.com>
2023-06-12 16:46:18 +0800

    style 2

--- src/shop/Main.jsx
+++ src/shop/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Menu from './Menu';
 import Nav from './Nav';
+import Style from './Style';
 
 const Main = () => {
   return (
     <>
       <Nav />
       <Menu />
+      <Style />
     </>
   );
 };
